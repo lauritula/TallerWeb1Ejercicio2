85d508c1e9e573e598a795b3a59a38937fb49132
Laura <mlaura.tula@gmail.com>
2015-04-22 11:46:56 -0300

    Test de Depósito
    Funciona.

--- Tarea_20150416/test/ar/edu/tallerweb/cuenta/CuentaBancariaTest.java
+++ Tarea_20150416/test/ar/edu/tallerweb/cuenta/CuentaBancariaTest.java
@@ -1,10 +1,16 @@
 package ar.edu.tallerweb.cuenta;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+
 public class CuentaBancariaTest {
 	@Test
 	public void depositoValido() {
-		CuentaBancaria miCuenta = new CuentaBancaria(0);
+		CuentaBancaria miCuenta = new CuentaBancaria(1000);
+		miCuenta.depositarDinero(miCuenta.saldo, 300);
+		Assert.assertTrue(true);
+		Assert.assertTrue(miCuenta.saldo == 1300);
 	}
 
 	
